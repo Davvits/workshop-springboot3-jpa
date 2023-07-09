19314af8d6bbf8a017105b6ba07451d80c29c56e
davi <kaliflamel@gmail.com>
2023-07-08 23:19:52 -0300

    Update Entity : User

--- src/main/java/com/practice/course/entities/User.java
+++ src/main/java/com/practice/course/entities/User.java
@@ -1,16 +1,21 @@
 package com.practice.course.entities;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
 @Entity
-@Table(name = "tb_users")
+@Table(name = "tb_user")
 public class User implements Serializable{
 	private static final long serialVersionUID = 1L;
 	@Id
@@ -20,6 +25,10 @@ public class User implements Serializable{
 	private String email;
 	private String phone;
 	private String password;
+	
+	@JsonIgnore
+	@OneToMany(mappedBy = "client")
+	private List<Order>orders = new ArrayList<>();
 	
 	public User() {
 	}
@@ -89,6 +98,11 @@ public class User implements Serializable{
 	public void setPassword(String password) {
 		this.password = password;
 	}
+
+	public List<Order> getOrders() {
+		return orders;
+	}
+	
 	
 	
 }
