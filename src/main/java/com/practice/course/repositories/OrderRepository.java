1bd6d9a307bd4f85a141113cf914a017fe9119b2
davi <kaliflamel@gmail.com>
2023-07-08 23:16:46 -0300

    Create new Repository : Order

---
+++ src/main/java/com/practice/course/repositories/OrderRepository.java
@@ -0,0 +1,10 @@
+package com.practice.course.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.practice.course.entities.Order;
+
+
+public interface OrderRepository extends JpaRepository<Order, Long>{
+
+}
